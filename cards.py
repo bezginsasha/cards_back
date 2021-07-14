90535106658fd4e91e472bc10fc1074ad0fdc9d8
bezgin sasha <bezgin.sasha06@gmail.com>
2021-07-14 20:45:40 +0700

    edit index controller to return json from mongo in response

--- cards.py
+++ cards.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, Response, request
+from pymongo import MongoClient
+from bson import json_util
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return '<p>Hello world</p>'
+
+    client = MongoClient()
+    db = client.test
+    cards_collection = db.cards
+
+    words_cursor = cards_collection.find({})
+
+    words_list = list(words_cursor)
+
+    words_json = json_util.dumps(words_list)
+
+
+    resp = Response(words_json)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp

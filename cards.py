ef18020cbd45d3c9ba390199adcb0d87ecef63e4
bezgin sasha <bezgin.sasha06@gmail.com>
2021-07-24 18:14:50 +0700

    refactor: redo spaces to tabs

--- cards.py
+++ cards.py
@@ -8,32 +8,32 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
-    cards_collection = db.cards
+	cards_collection = db.cards
 
-    words_cursor = cards_collection.find({})
+	words_cursor = cards_collection.find({})
 
-    words_list = list(words_cursor)
+	words_list = list(words_cursor)
 
-    words_json = json_util.dumps(words_list)
+	words_json = json_util.dumps(words_list)
 
 
-    resp = Response(words_json)
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    return resp
+	resp = Response(words_json)
+	resp.headers['Access-Control-Allow-Origin'] = '*'
+	return resp
 
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
 
-    cards_collection = db.cards
+	cards_collection = db.cards
 
-    word = {
-        "original_word": request.form['original_word'],
-        "translated_word": request.form['translated_word']
-    }
+	word = {
+		"original_word": request.form['original_word'],
+		"translated_word": request.form['translated_word']
+	}
 
-    inserted_word = cards_collection.insert_one(word)
+	inserted_word = cards_collection.insert_one(word)
 
-    resp = Response(json.dumps({'id': str(inserted_word.inserted_id)}))
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    return resp
+	resp = Response(json.dumps({'id': str(inserted_word.inserted_id)}))
+	resp.headers['Access-Control-Allow-Origin'] = '*'
+	return resp

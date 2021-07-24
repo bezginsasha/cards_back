f7e426f64abc4446df202ff6192313dd4e224800
bezgin sasha <bezgin.sasha06@gmail.com>
2021-07-24 18:19:53 +0700

    change index controller to use cards service

--- cards.py
+++ cards.py
@@ -2,22 +2,14 @@ from flask import Flask, Response, request
 from db import db
 import json
 from bson import json_util
+from services.cards import get_all_cards
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
-	cards_collection = db.cards
-
-	words_cursor = cards_collection.find({})
-
-	words_list = list(words_cursor)
-
-	words_json = json_util.dumps(words_list)
-
-
-	resp = Response(words_json)
+	cards = get_all_cards()
+	resp = Response(cards)
 	resp.headers['Access-Control-Allow-Origin'] = '*'
 	return resp
 

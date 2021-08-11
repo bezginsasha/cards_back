081a7eb5480b841454c8affb5b6411832d9efa18
bezginsasha <bezgin.sasha06@gmail.com>
2021-08-11 19:24:58 +0700

    refactor: rewrite cards service for using class because conficts names of service and controllers

--- services/cards.py
+++ services/cards.py
@@ -4,34 +4,43 @@ from bson.objectid import ObjectId
 
 cards_collection = db.cards
 
-def get_clear_card_item(card):
-	"""
-	mongo has inconvenient format of id:
-	'_id': ObjectId("60fbd0c0588d65e6872dc118")
-	function get card object taken from mongo
-	and returns card object with pretty id:
-	'id': '60fbd0c0588d65e6872dc118'
-	"""
-	card['id'] = str(card['_id'])
-	card.pop('_id')
-	return card
 
+class CardsService:
+	def get_clear_card_item(self, card):
+		"""
+		mongo has inconvenient format of id:
+		'_id': ObjectId("60fbd0c0588d65e6872dc118")
+		function get card object taken from mongo
+		and returns card object with pretty id:
+		'id': '60fbd0c0588d65e6872dc118'
+		"""
+		card['id'] = str(card['_id'])
+		card.pop('_id')
+		return card
 
-def get_all_cards():
-	cards_cursor = cards_collection.find({})
-	all_cards = list(cards_cursor)
-	all_cards = [get_clear_card_item(card) for card in all_cards]
-	return json_util.dumps(all_cards)
+	def get_all_cards(self):
+		cards_cursor = cards_collection.find({"original_word": {'$exists': True}})
+		all_cards = list(cards_cursor)
+		all_cards = [self.get_clear_card_item(card) for card in all_cards]
+		return json_util.dumps(all_cards)
 
+	def insert_card(self, original_word, translated_word):
+		card = {
+			'original_word': original_word,
+			'translated_word': translated_word
+		}
+		inserted_card = cards_collection.insert_one(card)
+		return str(inserted_card.inserted_id)
 
-def insert_card(original_word, translated_word):
-	card = {
-		'original_word': original_word,
-		'translated_word': translated_word
-	}
-	insertedCard = cards_collection.insert_one(card)
-	return str(insertedCard.inserted_id)
+	def update_card(self, id, original_word, translated_word):
+		id = {'_id': ObjectId(id)}
+		card = {
+			'$set': {
+				'original_word': original_word,
+				'translated_word': translated_word
+			}
+		}
+		cards_collection.update_one(id, card)
 
-
-def delete_card(id):
-	cards_collection.delete_one({'_id': ObjectId(id)})
+	def delete_card(self, id):
+		cards_collection.delete_one({'_id': ObjectId(id)})

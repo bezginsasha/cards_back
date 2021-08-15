35e5d3f56a0ed6697be7f28f7b88449b75ab4d04
bezginsasha <bezgin.sasha06@gmail.com>
2021-08-15 19:48:23 +0700

    using humps to separate cases - in js - camel case, in python - snake case

--- views/cards.py
+++ views/cards.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request
 from bson import json_util
 from services.cards import get_all_cards_service, insert_card_service, update_card_service, delete_card_service
 from utils.decorators import standard_headers_with_str_response
+from humps import camelize
 
 cards_bp = Blueprint('cards', __name__, url_prefix='/api/cards')
 
@@ -10,14 +11,14 @@ cards_bp = Blueprint('cards', __name__, url_prefix='/api/cards')
 @standard_headers_with_str_response
 def get_all_cards():
 	cards = get_all_cards_service()
-	return cards
+	return json_util.dumps(camelize(cards))
 
 
 @cards_bp.route('/add', methods=['GET', 'POST'])
 @standard_headers_with_str_response
 def add_card():
-	original_word = request.form['original_word']
-	translated_word = request.form['translated_word']
+	original_word = request.form[camelize('original_word')]
+	translated_word = request.form[camelize('translated_word')]
 	inserted_id = insert_card_service(original_word, translated_word)
 	return json_util.dumps({'id': inserted_id})
 
@@ -35,7 +36,7 @@ def delete_card():
 def update_card():
 	update_card_service(
 		request.form['id'],
-		request.form['original_word'],
-		request.form['translated_word']
+		request.form[camelize('original_word')],
+		request.form[camelize('translated_word')]
 	)
 	return json_util.dumps({'result': 'ok'})
--- views/piles.py
+++ views/piles.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request
 from bson import json_util
 from services.piles import get_all_piles_service, insert_pile_service, delete_pile_service
 from utils.decorators import standard_headers_with_str_response
+from humps import camelize
 
 piles_bp = Blueprint('piles', __name__, url_prefix='/api/piles')
 
@@ -10,18 +11,18 @@ piles_bp = Blueprint('piles', __name__, url_prefix='/api/piles')
 @standard_headers_with_str_response
 def get_all():
 	piles = get_all_piles_service()
-	return piles
+	return json_util.dumps(camelize(piles))
 
 
 @piles_bp.route('/add', methods=['POST'])
 @standard_headers_with_str_response
 def insert_pile():
-	insert_pile_service(request.form['pile_name'])
+	insert_pile_service(request.form[camelize('pile_name')])
 	return json_util.dumps({'result': 'ok'})
 
 
 @piles_bp.route('/delete', methods=['POST'])
 @standard_headers_with_str_response
 def delete_pile():
-	delete_pile_service(request.form['pile_name'])
+	delete_pile_service(request.form[camelize('pile_name')])
 	return json_util.dumps({'result': 'ok'})

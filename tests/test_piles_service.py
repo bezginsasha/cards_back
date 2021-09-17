b3f9cbe7084ac0ff9aef25714bb6c3c90eef4775
bezginsasha <bezgin.sasha06@gmail.com>
2021-09-18 03:07:13 +0700

    first test for piles services

---
+++ tests/test_piles_service.py
@@ -0,0 +1,19 @@
+from services.piles import PilesService
+from db import test_db
+from tests import pile_name, username
+
+piles_service = PilesService(test_db)
+
+
+def test_insert_and_pile():
+    pile_insert_result = piles_service.insert_pile(
+        pile_name,
+        username
+    )
+    assert pile_insert_result['result'] == 'ok'
+
+    delete_pile_result = piles_service.delete_pile(
+        pile_name,
+        username
+    )
+    assert delete_pile_result['result'] == 'ok'

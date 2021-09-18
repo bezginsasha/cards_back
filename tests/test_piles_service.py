8365ce1cb8db7024d926a0cc82f96f39a4689422
bezginsasha <bezgin.sasha06@gmail.com>
2021-09-18 19:06:24 +0700

    write test for piles service - double insert, update and double update

--- tests/test_piles_service.py
+++ tests/test_piles_service.py
@@ -1,6 +1,7 @@
 from services.piles import PilesService
 from db import test_db
 from tests import pile_name, username
+from utils.constants import DB_OPERATION_RESULT
 
 piles_service = PilesService(test_db)
 
@@ -17,3 +18,86 @@ def test_insert_and_delete_pile():
         username,
     )
     assert delete_pile_result['result'] == 'ok'
+
+
+def test_double_insert_pile():
+    pile_insert_result = piles_service.insert_pile(
+        pile_name,
+        username,
+    )
+    assert pile_insert_result['result'] == 'ok'
+
+    pile_insert_result = piles_service.insert_pile(
+        pile_name,
+        username,
+    )
+    assert pile_insert_result['result'] == DB_OPERATION_RESULT['already_exists']
+
+    piles_service.delete_pile(
+        pile_name,
+        username,
+    )
+
+
+def test_update_pile():
+    pile_insert_result = piles_service.insert_pile(
+        pile_name,
+        username,
+    )
+    assert pile_insert_result['result'] == 'ok'
+
+    pile_name_for_update = 'second_pile_name'
+    pile_update_result = piles_service.update_pile(
+        pile_name,
+        pile_name_for_update,
+        username,
+    )
+    assert pile_update_result['result'] == 'ok'
+
+    updated_pile = piles_service.find_pile_by_name(pile_name_for_update, username)
+    assert updated_pile is not None
+
+    old_pile = piles_service.find_pile_by_name(pile_name, username)
+    assert old_pile is None
+
+    piles_service.delete_pile(
+        pile_name_for_update,
+        username,
+    )
+
+
+def test_update_pile_with_existing_pile_name():
+    piles_service.insert_pile(
+        pile_name,
+        username,
+    )
+
+    second_pile_name = 'second_pile_name'
+    piles_service.insert_pile(
+        second_pile_name,
+        username,
+    )
+
+    pile_name_for_update = 'pile_name_for_update'
+    pile_update_result = piles_service.update_pile(
+        pile_name,
+        pile_name_for_update,
+        username,
+    )
+    assert pile_update_result['result'] == 'ok'
+
+    pile_update_result = piles_service.update_pile(
+        pile_name_for_update,
+        second_pile_name,
+        username,
+    )
+    assert pile_update_result['result'] == DB_OPERATION_RESULT['already_exists']
+
+    piles_service.delete_pile(
+        second_pile_name,
+        username,
+    )
+    piles_service.delete_pile(
+        pile_name_for_update,
+        username,
+    )
